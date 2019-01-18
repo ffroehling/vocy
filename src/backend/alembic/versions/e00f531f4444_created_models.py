eca279e110499a71a1f513847e739476cdd9e0e3
Felix Fröhling <felix.froehling1@gmail.com>
2019-01-18 11:24:04 +0100

    Alembic integrated and .gitignore updated
    Added Alembic
    
    Updated Gitignore

---
+++ src/backend/alembic/versions/e00f531f4444_created_models.py
@@ -0,0 +1,24 @@
+"""created models
+
+Revision ID: e00f531f4444
+Revises: 
+Create Date: 2019-01-18 11:10:31.098558
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e00f531f4444'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
